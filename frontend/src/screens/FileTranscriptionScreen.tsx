155e2045775e49b9a622fc27985c0ac1602334a6
Jose L <jolacdev@users.noreply.github.com>
2025-12-14 14:31:29 +0100

    feat: add FileTranscriptionScreen to run the file transcription

--- frontend/src/screens/FileTranscriptionScreen.tsx
+++ frontend/src/screens/FileTranscriptionScreen.tsx
@@ -1,3 +1,46 @@
-const FileTranscriptionScreen = () => <>FileTranscriptionScreen</>;
+import { useEffect, useState } from 'react';
+
+import TranscriptionLoader from '@features/transcription/components/TranscriptionLoader';
+import useAppStore, { APP_VIEWS } from '@store/useAppStore';
+import { TranscriptionSegment } from 'types/pywebview/pywebview-api';
+
+const FileTranscriptionScreen = () => {
+  const file = useAppStore((state) => state.transcriptionFile);
+  const setView = useAppStore((state) => state.setView);
+
+  // TODO: Move to the store
+  const [segments, setSegments] = useState<TranscriptionSegment[]>();
+
+  // TODO: Remove
+  useEffect(() => {
+    console.log({ segments });
+  }, [segments]);
+
+  useEffect(() => {
+    const handleRunTranscription = async (
+      path: string,
+      model: string = 'base',
+    ) => {
+      try {
+        const transcribedSegments =
+          await window.pywebview.api.run_transcription(path, model);
+
+        setSegments(transcribedSegments);
+      } catch (_error) {
+        // TODO: Show alert error.
+        setView(APP_VIEWS.SELECTION);
+      }
+    };
+
+    if (!file?.absolutePath) {
+      setView(APP_VIEWS.SELECTION);
+      return;
+    }
+
+    handleRunTranscription(file.absolutePath);
+  }, [file, setView]);
+
+  return <TranscriptionLoader />;
+};
 
 export default FileTranscriptionScreen;
